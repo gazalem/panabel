4e5be7a5945fc368b941d69b241ed696b09d95b0
Alan Montoya <mon21033@byui.edu>
2023-05-11 11:17:15 -0500

    Guerlain autohide options

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,14 @@ const apiURL = "https://api.convertkit.com/v3/forms/4949448/subscribe";
 
 myForm.addEventListener("submit", (event) => {
     // Evita que el formulario se auto recargue
-    event.preventDefault();
-    
+    // event.preventDefault();
+
     // busca la data del formulario
     const formData = new URLSearchParams();
     for (const pair of new FormData(myForm)) {
         formData.append(pair[0], pair[1]);
     }
-    
+
     // add the tag formData for the object
     formData.set("tags", [3709979]);
 
@@ -38,6 +38,19 @@ myForm.addEventListener("submit", (event) => {
         },
         body: JSON.stringify(data)
     })
-    .then(console.log("Data Enviada Éxitosamente!"));
+        .then(console.log("Data Enviada Éxitosamente!"));
 
 });
+
+function unhide() {
+    const guerlainElement = document.querySelector("#exclusivoGuerlain");
+    const radioGroup = document.getElementsByName("exclusivo_guerlain");
+    if (guerlainElement.style.display === "block") {
+        guerlainElement.style.display = "none";
+        for (let i = 0; i < radioGroup.length; i++) {
+            radioGroup[i].checked = false;
+        }
+    } else {
+        guerlainElement.style.display = "block";
+    }
+}
